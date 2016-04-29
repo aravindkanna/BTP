85c18d7de4377d02b8837d51ebadc821db6c9e7d
Aravind Reddy <aravindreddy255@gmail.com>
2016-04-30 00:41:30 +0530

    added search() function

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <set>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,10 +11,28 @@ int main(){
 	s.insert(1);
 	s.insert(2);
 
-	set<int>::iterator it = s.begin();
+	/*set<int>::iterator it = s.begin();
 	for(it=s.begin();it!=s.end();it++){
 		cout << *it << "     " ;
 	}
+	cout << endl;*/
+
+	int m = 5;
+	int n = 10;
+	vector<vector<int> > vec(5, vector<int> (10));
+	vector<int> v(10);
+
+	for(int i=0;i<10;i++){
+		cout << v[i] << "    ";
+	}
 	cout << endl;
+
+	/*for(int i=0;i<vec.size();i++){
+		for(int j=0;j<vec[i].size();j++){
+			cout << vec[i][j] << "    ";
+		}
+		cout << endl;
+	}*/
+
 	return 0;
 }
--- project.cpp
+++ project.cpp
@@ -10,27 +10,70 @@
 
 using namespace std;
 
+void modify(vector<vector<int> > &mat, int size){
+	for(int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			if(mat[i][j]){
+				mat[i][j] = 1;
+			}
+		}
+	}
+}
+
+void search(vector<vector<int> > mat, int size, 
+	int v0, int d, vector<int> &vec, vector<int> &reach){
+	//base case
+	d = d + 1;
+	reach[v0] = 1;
+	if(d == 1){
+		for(int i=0;i<size;i++){
+			if(mat[v0][i] && !reach[v0]){
+				vec.push_back(i);
+				reach[i] = 1;
+			}
+		}
+		return;
+	}
+
+	for(int i=0;i<size;i++){
+		if(mat[v0][i] && i!=v0 && !reach[i]){
+			vec.push_back(i);
+			reach[i] = 1;
+			search(mat, size, i, d-1, vec, reach);
+		}
+	}
+	vec.push_back(v0);
+}
+
 int rootNlogN(int n){
-	return sqrt(n)*log(n);
+	return ceil(sqrt(n)*log(n));
 }
 
 int rootN(int n){
-	return sqrt(n);
+	return ceil(sqrt(n));
 }
 
 int logN(int n){
-	return log(n);
+	return ceil(log(n));
 }
 
-vector<int> get_distinguished(int size, int s1){
+
+/***
+	This function gives you a vector of distinguished nodes of the size we want.
+	num_nodes : representation for total number of nodes
+	num_dist  : representation for number of distinguished nodes
+	returns a vector of some integers which we can consider as distinguished nodes.
+	As set stores in sorted order they come out to be sorted
+***/
+vector<int> get_distinguished(int num_nodes, int num_dist){
 	
 	set<int> s;
 	srand(time(NULL));
 	while(1){
 		//srand(time(NULL));
-		int x = rand()%size;
+		int x = rand()%num_nodes;
 		s.insert(x);
-		if(s.size() == s1){
+		if(s.size() == num_dist){
 			break;
 		}
 	}
@@ -63,13 +106,77 @@ vector<vector<int> > B1(vector<vector<int> > mat, int size) {
 }
 
 
-
+/***
+	This function returns a matrix 
+	Can call that transitive closure of s nodes specified in S, searched through
+	a distance of d, with n nodes and e edges.
+	The matrix returned gives you an idea of what nodes are reachable from S to 
+	a distance of d.
+***/
 vector<vector<int> > R2(vector<vector<int> > mat, int size, 
 	vector<int> S, int d, int n, int e){
+
+	int s = S.size();
 	vector<vector<int> > res(size, vector<int> (size));
 
 	int s1 = rootNlogN(size);
 	vector<int> S1 = get_distinguished(size, s1);
+	//S1 is the set of rootNlogN distinguished nodes
+
+	vector<vector<int> > res1(size, vector<int> (size));
+	int search_dist = ceil(size/s1);
+	for(int i=0;i<s1;i++){
+		vector<int> vec;
+		vector<int> reach(size);
+		search(mat, size, S1[i], search_dist, vec, reach);
+
+		
+	}
+
+
+
+	/*//Base case
+	if(d==1){
+		for(int i=0;i<s;i++){
+			for(int j=0;j<size;j++){
+				if(mat[S[i]][j]){
+					res[S[i]][j] = 1;
+				}
+			}
+		}
+		return res;
+	}
+	else{
+			//first step
+		 	vector<vector<int> > res1(size, vector<int> (size));
+		 	res1 = R2(mat, size, S1, ceil(size/s1), s1, e);
+
+		 	//second step : Finding the transitive closure for res2
+		 	vector<vector<int> > res2(size, vector<int> (size));
+		 	res2 = B1(res1, size);
+
+		 	//adding the edges found in second step to original graph
+		 	for(int i=0;i<size;i++){
+		 		for(int j=0;j<size;j++){
+		 			if(mat[i][j] || res2[i][j]){
+		 				mat[i][j] = 1;
+		 			}
+		 		}
+		 	}
+
+		 	//third step
+		 	vector<vector<int> > res3(size, vector<int> (size));
+		 	res3 = R2(mat, size, S, ceil(size/s1), s1, e);
+
+		 	return res3;*/
+
+		 	/*   R2(mat, size, S1, ceil(size/s1), n, e) + 
+			   R2(mat, size, S1, ceil((d*s1)/size), s1, s1*s1) +
+			   R2(mat, size, S, ceil(size/s1), n, e+s1*s1);*/
+
+
+
+	
 	return res;
 }
 
@@ -84,6 +191,16 @@ void print_mat(vector<vector<int> > mat, int size){
 }
 
 int main(){
+
+	/***
+		Hexagon Input
+		0 1 0 0 0 1
+		1 0 1 0 0 0
+		0 1 0 1 0 0
+		0 0 1 0 1 0
+		0 0 0 1 0 1
+		1 0 0 0 1 0
+	***/
 
 	int N;
 	cin >> N;
@@ -100,10 +217,12 @@ int main(){
 		}
 	}
 
+	modify(mat, N);
+
 	vector<vector<int> > b1(N, vector<int> (N));
 	b1 = B1(mat, N);
 
-	print_mat(b1, N);
+	//print_mat(b1, N);
 	//cout << rootNlogN(24) << endl;
 
 	/*vector<int> v = get_distinguished(N, rootNlogN(N));
@@ -112,5 +231,21 @@ int main(){
 		cout << v[i] << "   ";
 	}
 	cout << endl;*/
+
+	vector<int> S;
+	for(int i=0;i<N;i++){
+		S.push_back(i);
+	}
+	/*vector<vector<int> > r2(N, vector<int> (N));
+	r2 = R2(mat, N, S, 2, N, N);
+	print_mat(r2, N);*/
+
+	vector<int> vec;
+	vector<int> reach(N);
+	search(mat, N, 2, 2, vec, reach);
+	for(int i=0;i<vec.size();i++){
+		cout << vec[i] << "   ";
+	}
+
 	return 0;
 }
