8c0ce4e523c6f4e0e12ab1d6c8038fa358de3bc7
aravindkanna <aravindreddy255@gmail.com>
2016-08-10 01:27:30 +0530

    fixed a small bug

--- project.cpp
+++ project.cpp
@@ -11,6 +11,11 @@
 
 using namespace std;
 
+void rem_dup(vector<int> &v){
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+}
+
 void modify(vector<vector<int> > &mat, int size){
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++){
@@ -31,14 +36,15 @@ void print_mat(vector<vector<int> > mat, int size){
 	}
 }
 
+//tested..!!!
 void search(vector<vector<int> > mat, int size, 
 	int v0, int d, vector<int> &vec, vector<int> &reach){
-	//base case
-	//d = d + 1;
+	
 	reach[v0] = 1;
+	vec.push_back(v0);
 	if(d == 1){
-		for(int i=0;i<size;i++){
-			if(mat[v0][i] && !reach[v0]){
+		for(int i = 0;i < size;i++){
+			if(mat[v0][i] == 1 && reach[i] == 0){
 				vec.push_back(i);
 				reach[i] = 1;
 			}
@@ -46,19 +52,14 @@ void search(vector<vector<int> > mat, int size,
 		return;
 	}
 
-	for(int i=0;i<size;i++){
-		if(mat[v0][i] && i!=v0 && !reach[i]){
+	for(int i = 0;i < size;i++){
+		if(mat[v0][i] && i != v0 && !reach[i]){
 			vec.push_back(i);
 			reach[i] = 1;
-			search(mat, size, i, d-1, vec, reach);
+			search(mat, size, i, d - 1, vec, reach);
 		}
 	}
-	vec.push_back(v0);
-}
-
-void rem_dup(vector<int> &v){
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
+	rem_dup(vec);
 }
 
 int rootN(int n){
@@ -70,13 +71,6 @@ int logN(int n){
 }
 
 int rootNlogN(int n){
-	/*int x = rootN(n)*logN(n); 
-	if(x == n && x>0){
-		return x-1;
-	}
-	else{
-		return x;
-	}*/
 	return ceil(sqrt(n)*log(n));
 }
 
@@ -88,6 +82,7 @@ int rootNlogN(int n){
 	num_dist  : representation for number of distinguished nodes
 	returns a vector of some integers which we can consider as distinguished nodes.
 	As set stores in sorted order they come out to be sorted
+	It is tested and working fine
 ***/
 vector<int> get_distinguished(int num_nodes, int num_dist){
 	
@@ -110,17 +105,17 @@ vector<vector<int> > B1(vector<vector<int> > mat, int size) {
 	vector<vector<int> > res(size, vector<int> (size));
 
 	#pragma omp parallel for private(i, j)
-	for(int i=0;i<size;i++){
-		for(int j=0;j<size;j++){
+	for(int i = 0;i < size;i++){
+		for(int j = 0;j < size;j++){
 			res[i][j] = mat[i][j];
 		}
 	}
 
 
-	for(int k=0;k<size;k++){
+	for(int k = 0;k < size;k++){
 		#pragma omp parallel for private(i, j)
-		for(int i=0;i<size;i++){
-			for(int j=0;j<size;j++){
+		for(int i = 0;i < size;i++){
+			for(int j = 0;j < size;j++){
 				res[i][j] = res[i][j] || (res[i][k] && res[k][j]);
 			}
 		}
@@ -151,10 +146,10 @@ vector<vector<int> > R2(vector<vector<int> > mat, int size,
 	//for other nodes.
 	vector<vector<int> > res1(size, vector<int> (size));
 	int search_dist = ceil(size/s1);
-	for(int i=0;i<s1;i++){
+	for(int i = 0;i < s1;i++){
 		vector<int> vec;
 		vector<int> reach(size);
-		search(mat, size, S1[i], search_dist+1, vec, reach);
+		search(mat, size, S1[i], search_dist + 1, vec, reach);
 		rem_dup(vec);
 
 		int x = vec.size();
@@ -194,49 +189,6 @@ vector<vector<int> > R2(vector<vector<int> > mat, int size,
 			res[S[i]][vec[j]] = 1;
 		}
 	}
-
-
-	/*//Base case
-	if(d==1){
-		for(int i=0;i<s;i++){
-			for(int j=0;j<size;j++){
-				if(mat[S[i]][j]){
-					res[S[i]][j] = 1;
-				}
-			}
-		}
-		return res;
-	}
-	else{
-			//first step
-		 	vector<vector<int> > res1(size, vector<int> (size));
-		 	res1 = R2(mat, size, S1, ceil(size/s1), s1, e);
-
-		 	//second step : Finding the transitive closure for res2
-		 	vector<vector<int> > res2(size, vector<int> (size));
-		 	res2 = B1(res1, size);
-
-		 	//adding the edges found in second step to original graph
-		 	for(int i=0;i<size;i++){
-		 		for(int j=0;j<size;j++){
-		 			if(mat[i][j] || res2[i][j]){
-		 				mat[i][j] = 1;
-		 			}
-		 		}
-		 	}
-
-		 	//third step
-		 	vector<vector<int> > res3(size, vector<int> (size));
-		 	res3 = R2(mat, size, S, ceil(size/s1), s1, e);
-
-		 	return res3;*/
-
-		 	/*   R2(mat, size, S1, ceil(size/s1), n, e) + 
-			   R2(mat, size, S1, ceil((d*s1)/size), s1, s1*s1) +
-			   R2(mat, size, S, ceil(size/s1), n, e+s1*s1);*/
-
-
-
 	
 	return res;
 }
@@ -270,38 +222,16 @@ int main(){
 	}
 
 	modify(mat, N);
-
-	vector<vector<int> > b1(N, vector<int> (N));
-	//b1 = B1(mat, N);
-
-	//print_mat(b1, N);
-	//cout << rootNlogN(24) << endl;
-
-	/*vector<int> v = get_distinguished(N, rootNlogN(N));
-	cout << v.size() << endl;
-	for(int i=0;i<v.size();i++){
-		cout << v[i] << "   ";
-	}
-	cout << endl;*/
-
-	cout << rootNlogN(2) << endl;
-
-	vector<int> S;
-	for(int i=0;i<N;i=i+2){
-		S.push_back(i);
-	}
-	vector<vector<int> > r2(N, vector<int> (N));
-	r2 = R2(mat, N, S, 2, N, N);
-	print_mat(r2, N);
+	print_mat(mat, N);
 
 	vector<int> vec;
-	vector<int> reach(N);
-	search(mat, N, 2, 3, vec, reach);
-	rem_dup(vec);
-	for(int i=0;i<vec.size();i++){
-		cout << vec[i] << "   ";
-	}
-	cout << endl;
+	vector<int> reach(N, 0);
+	search(mat, N, 3, 2, vec, reach);
 
+	cout << endl << endl;
+	for(int i = 0;i < vec.size();i++) {
+		cout << vec[i] << "    ";
+	}
+	cout <<endl;
 	return 0;
 }
